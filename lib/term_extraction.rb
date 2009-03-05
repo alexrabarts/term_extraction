5ac21562f1861b3621839b0e4c003f90b5786bb2
alex <alexrabarts@gmail.com>
2009-03-05 15:43:52 +1100

    Allow context and API key to be set after initialization

--- lib/term_extraction.rb
+++ lib/term_extraction.rb
@@ -1,4 +1,6 @@
 class TermExtraction
+  attr_accessor :context, :api_key
+
   def initialize(options={})
     @context = options[:context]
     @api_key = options[:api_key]
--- test/term_extraction_test.rb
+++ test/term_extraction_test.rb
@@ -4,12 +4,35 @@ class TermExtractionTest < Test::Unit::TestCase
   should 'return correct terms from Yahoo!' do
     yahoo = TermExtraction::Yahoo.new
     yahoo.stubs(:remote_xml).returns(read_xml_fixture('yahoo'))
-    assert_equal yahoo.terms, ['gears of war', 'gears']
+    assert_equal yahoo.terms, correct_yahoo_terms
   end
   
   should 'return correct terms from Zemanta' do
     zemanta = TermExtraction::Zemanta.new
     zemanta.stubs(:remote_xml).returns(read_xml_fixture('zemanta'))
-    assert_equal zemanta.terms, ['Apple', 'IMac', 'Rumor', 'Hardware', 'Nvidia', 'Macintosh', 'Desktops', 'AllInOne']
+    assert_equal zemanta.terms, correct_zemanta_terms
   end
+
+  should 'be able to set the context after initialization' do
+    yahoo         = TermExtraction::Yahoo.new
+    context       = 'foo'
+    yahoo.context = context
+    assert_equal yahoo.context, context
+  end
+
+  should 'be able to set the api key after initialization' do
+    zemanta         = TermExtraction::Zemanta.new
+    context         = 'bar'
+    zemanta.context = context
+    assert_equal zemanta.context, context
+  end
+
+  private
+    def correct_yahoo_terms
+      ['gears of war', 'gears']
+    end
+
+    def correct_zemanta_terms
+      ['Apple', 'IMac', 'Rumor', 'Hardware', 'Nvidia', 'Macintosh', 'Desktops', 'AllInOne']
+    end
 end
